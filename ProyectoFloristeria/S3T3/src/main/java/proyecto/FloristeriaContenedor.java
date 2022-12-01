b73cd009904b2283612ef879fadb494e731e273f
Ismael <Ismael.Tortosa@hotmail.com>
2022-12-01 16:04:52 +0100

    Primera rama Isma

--- ProyectoFloristeria/S3T3/src/main/java/proyecto/FloristeriaContenedor.java
+++ ProyectoFloristeria/S3T3/src/main/java/proyecto/FloristeriaContenedor.java
@@ -1,5 +1,40 @@
 package proyecto;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FloristeriaContenedor {
 
+	static Scanner entrada = new Scanner(System.in);
+	
+	private static ArrayList<Floristeria> floristerias = new ArrayList<Floristeria>();
+	
+	//metodo crear floristería
+	public void crearFloristeria(Floristeria floristeria1) {
+		
+		String nombre;
+		
+		System.out.println("Introducir nombre para la nueva floristería: ");
+		nombre = entrada.next();
+		
+		for ( int i = 0; i < floristerias.size(); i++) {
+			
+			while(floristerias.get(i).getNombre().equals(nombre)) {
+			
+				if (floristerias.get(i).getNombre().equalsIgnoreCase(nombre)) {
+				System.out.println("El nombre introducido ya existe.");
+				
+				System.out.println("Introducir el nombre para la nueva floristería: ");
+				nombre = entrada.next();
+				} 
+			}
+		}
+		
+		floristeria1 = new Floristeria(nombre);
+		
+		floristerias.add(floristeria1);
+		
+		System.out.println("La floristería " + floristeria1.getNombre() + " se ha creado correctamente.");
+		
+	}
 }
--- ProyectoFloristeria/S3T3/src/test/java/proyecto/FloristeriaContenedorTest.java
+++ ProyectoFloristeria/S3T3/src/test/java/proyecto/FloristeriaContenedorTest.java
@@ -1,14 +1,39 @@
 package proyecto;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class FloristeriaContenedorTest {
+	
+	static ArrayList<Floristeria> contenedor = new ArrayList<Floristeria>();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void crearFloristeria() {
+			
+			String nombre = "navarro";
+			
+			for ( int i = 0; i < contenedor.size(); i++) {
+				
+				while(contenedor.get(i).getNombre().equals(nombre)) {
+				
+					if (contenedor.get(i).getNombre().equalsIgnoreCase(nombre)) {
+					System.out.println("El nombre introducido ya existe.");
+					
+					} 
+				}
+			}
+		}
+	
+	@Test
+	public void verArboles() {
+		
+		if (contenedor.size() > 0)
+			for (Floristeria ver : contenedor) {
+				System.out.println(ver);
+			} else {
+				System.out.println("ERROR");
+			}
 	}
 
 }
